00fad2ddf1a183ae8a9f4fcfdbed727dda00cb5f
Sandro Felipe <sandrokbca@hotmail.com>
2021-02-28 21:21:19 -0300

    Initial

---
+++ src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../context/ChallengeContext'
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile(){
+    const {level} = useContext(ChallengesContext)
+    return(
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/osandrofelipe.png" alt="Sandro Felipe"/>
+            <div>
+                <strong>Sandro Felipe</strong>
+                
+                <p>
+                <img src="icons/level.svg" alt="Level"/>
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    )
+}
+
